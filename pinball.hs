1032c7073890f61bcde2df04cf5210633fe6ef87
matsakelarsson <m.larsson88@gmail.com>
2017-03-13 19:24:14 +0100

    pinball.hs added

---
+++ pinball.hs
@@ -0,0 +1,72 @@
+import Prelude hiding (Left, Right, flip)
+import Test.HUnit hiding (Path)
+
+data Cell = Empty | Left | Right
+  deriving (Show, Eq)
+
+type Grid = [[Cell]]
+
+data Direction = North | South | East | West
+  deriving (Show, Eq)
+
+type Border = (Direction, Int)
+
+type Position = (Int, Int)
+
+type Path = [Position]
+
+validGrid :: Grid -> Bool
+validGrid _ = True
+
+validEntryPoint :: Grid -> Border -> Bool
+validEntryPoint _ _ = True
+
+trajectory :: Grid -> Border -> Path
+trajectory _ _ = []
+
+play :: Grid -> Border -> Border
+play _ b = b
+
+
+-- HUnit Test Cases
+
+-- data for test cases
+
+goodgrid :: Grid
+goodgrid = [[Empty, Left,  Empty, Empty, Right],
+            [Left,  Right, Empty, Empty, Left ],
+            [Empty, Empty, Right, Empty, Right],
+            [Right, Empty, Empty, Empty, Empty],
+            [Empty, Empty, Empty, Left,  Empty]]
+
+badgrid :: Grid
+badgrid = [[Empty, Left,  Empty, Empty, Right],
+           [Left,  Right, Empty, Empty, Left ],
+           [Empty, Empty, Right, Empty],
+           [Right, Empty, Empty, Empty, Empty],
+           [Empty, Empty, Empty, Left,  Empty]]
+
+-- test cases
+test0 = TestCase $ assertBool "Valid Grid"
+                   (validGrid goodgrid)
+
+test1 = TestCase $ assertBool "Invalid Grid"
+                   (not (validGrid badgrid))
+
+test2 = TestCase $ assertBool "Valid Entry Point"
+                   (validEntryPoint goodgrid (South, 3))
+
+test3 = TestCase $ assertBool "Invalid Entry Point"
+                   (not (validEntryPoint goodgrid (South, 12)))
+
+test4 = TestCase $ assertEqual "Play (South, 3)"
+                   (South, 1) (play goodgrid (South, 2))
+
+
+test5 = TestCase $ assertEqual "Trajectory (South, 2)" traj (trajectory goodgrid (South, 2))
+  where traj = [(2, 4), (2, 3), (2, 2), (3, 2), (4, 2), (4, 1),
+                (3, 1), (2, 1), (1, 1), (1, 2), (1, 3), (1, 4)]
+
+runTests = runTestTT $ TestList [test0, test1, test2, test3, test4, test5]
+
+extraTests = rutTestTT $ TestList [] -- Add your tests to this list
